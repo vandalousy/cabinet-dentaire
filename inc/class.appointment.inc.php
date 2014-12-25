78d251af219911ffaf228ceb729dc1851ea5cdec
vandalousy <vandalousy@gmail.com>
2014-12-25 15:19:07 +0100

    Create class.appointment.inc.php

---
+++ inc/class.appointment.inc.php
@@ -0,0 +1,13 @@
+<?php 
+
+class appointment{ 
+	protected $classTable='appointment';
+	protected $appId;
+	protected $starTime;
+	protected $endTime;
+	protected $appDate;
+	protected $reason;
+	protected $patientId;
+}
+
+?>
