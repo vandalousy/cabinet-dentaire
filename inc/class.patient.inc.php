53f7341a743ad64b4eb3ea9be8bb9865a3816a01
vandalousy <vandalousy@gmail.com>
2014-12-25 15:20:34 +0100

    Create class.patient.inc.php

---
+++ inc/class.patient.inc.php
@@ -0,0 +1,14 @@
+<?php 
+
+class patient{ 
+	protected $classTable='patient';
+	protected $patientId;
+	protected $firstName;
+	protected $lastName;
+	protected $adress;
+	protected $postCode;
+	protected $gender;
+	protected $dateOfBirth;
+	protected $telephone;
+}
+?>
